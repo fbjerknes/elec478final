23491ab2d62602e12d3100029d94f380d23334c1
Alex Berlaga <awb4@rice.edu>
2021-12-13 17:41:07 -0600

    items

--- main.py
+++ main.py
@@ -6,19 +6,50 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score
 import math
 
-def load_data():
-    ####REPLACE THESE WITH THE ACTUAL DATASET LOADING
-    pass
+items1 = ["Molecular Weight", "Targets", "Bioactivities", "AlogP", "Polar Surface Area", "HBA", "HBD",
+                            "#RO5 Violations", "#Rotatable Bonds", "QED Weighted", "CX Acidic pKa", "CX Basic pKa",
+                            "CX LogP", "CX LogD", "Aromatic Rings", "Heavy Atoms", "Molecular Species"]
+items2 = ["Molecular Weight", "Targets", "Bioactivities", "AlogP", "Polar Surface Area", "HBA", "HBD",
+                            "#RO5 Violations", "#Rotatable Bonds", "QED Weighted",
+                            "CX LogP", "CX LogD", "Aromatic Rings", "Heavy Atoms", "Molecular Species"]
+def load_data(it):
+    if it:
+        ite = items1
+    else:
+        ite = items2
+    dfx = pd.read_csv('drugdata.csv', delimiter=";")
+    dfx = dfx.filter(items=ite)
+    dfx = dfx.mask(dfx.eq("None")).dropna()
+    dfy = dfx["Molecular Species"]
+    dfx = dfx.filter(items=ite)
+    print(dfx)
+    y_data = []
+    for row in dfy:
+        if row == "NEUTRAL":
+            y_data.append(0)
+        elif row == "BASE":
+            y_data.append(1)
+        elif row == "ACID":
+            y_data.append(2)
+        elif row == "ZWITTERION":
+            y_data.append(3)
+        else:
+            y_data.append(-1)
+    return dfx.values[:250000], y_data[:250000]
 
 data_X, data_y = load_data()
-tv_X, test_X, tv_y, test_y = train_test_split(data_X, data_y, 0.15)
-train_X, valid_X, train_y, valid_y = train_test_split(tv_X, tv_y, 0.2)
+tv_X, test_X, tv_y, test_y = train_test_split(data_X, data_y, test_size=0.15)
+train_X, valid_X, train_y, valid_y = train_test_split(tv_X, tv_y, test_size=0.2)
+
+
+
 
 def get_best_rfc():
-    sizes = [10, 25, 100, 250, 1000]
+    sizes = [10, 25, 100]
     criteria = ["gini", "entropy"]
 
     max_depth = [5, 10, 50, None]
@@ -34,7 +65,13 @@ def get_best_rfc():
                 if valid_error < min_valid_error:
                     min_valid_error = valid_error
                     best_rfc = rfc
+                print(size)
     test_error = mean_squared_error(test_y, best_rfc.predict(test_X))
     return best_rfc, test_error
 
+rfc, error = get_best_rfc()
+
+print(error)
+print(rfc.feature_importances_)
+print(pca_features())
 
